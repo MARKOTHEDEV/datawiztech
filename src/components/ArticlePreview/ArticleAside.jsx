d8048328fc97e7f5c8a4975f95ee6e4b8f22c85c
markothedev <markothedevmail@gmail.com>
2024-11-24 17:51:42 +0100

    fixed the aside article

--- src/components/ArticlePreview/ArticleAside.jsx
+++ src/components/ArticlePreview/ArticleAside.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import like from "../../assets/images/icons8-facebook-like-4qo.png";
 import download from "../../assets/images/icons8-download-from-the-cloud-dqs.png";
 import share from "../../assets/images/icons8-forward-arrow-100-2-F53.png";
@@ -8,6 +8,7 @@ import { GoStar, GoStarFill } from "react-icons/go";
 import { Link, useParams, useSearchParams } from "react-router-dom";
 
 const ArticleAside = ({ articles ,setArticle}) => {
+  const [selectedItem,setSelectedItem] = useState(null)
   const profilepic =
     "https://firebasestorage.googleapis.com/v0/b/datawiztech-9a46a.appspot.com/o/profilepic%2Fprofile-circle.png?alt=media&token=ec19eaec-b6f7-472d-8fc4-affdbd330f78";
   const truncateText = (text, maxLength) => {
@@ -40,13 +41,74 @@ const ArticleAside = ({ articles ,setArticle}) => {
   let [searchParams, setSearchParams] = useSearchParams();
   const searchTerm =searchParams.get('searchTerm')
 
+  useEffect(()=>{
+    const id = searchParams.get('id');
+      if(id&&articles){
+        setSelectedItem(articles.find(item=>item.id===id))
+      }
+  },[searchParams])
 
   return (
     <div
       className="px-3 overflow-y-auto scrollbar-design pt-3"
       style={{ maxHeight: "120vh" }}
     >
-      {articles?.map((data, index) => (
+      {
+        selectedItem?
+        <div className="pb-1 " style={{'cursor':'pointer'}}
+        onClick={()=>{
+          if(setArticle){
+            setSearchParams({'id':selectedItem?.id,'searchTerm':searchTerm})
+            setArticle(selectedItem)
+
+          }
+        }}
+        >
+          
+          <div className={`search-result-card ${selectedItem?.id === searchParams.get('id') ? "active" : ""}`}>
+            <Link
+              className="search-card-title pb-3"
+            >
+              {selectedItem?.title}
+            </Link>
+            <div class="search-card-profile">
+             
+              <div class="search-card-flex">
+                <div class="search-card-pic-container">
+                  <div class="search-card-pic">
+                    <img
+                      src={
+                        profilepic
+                      }
+                      data-bs-toggle="collapse"
+                      href={"#dataCollapseProfile" + selectedItem?.id}
+                      role="button"
+                      aria-expanded="false"
+                      aria-controls={"dataCollapseProfile" + selectedItem?.id}
+                      alt=".."
+                      className="img-fluid search-card-pic"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="search-card-amount">
+                {!selectedItem?.price || selectedItem?.price === 0 ? "Free" : `N ${selectedItem?.price}`}
+              </div>
+            </div>
+            <Link
+              to={`/search/article/result/${selectedItem?._id}`}
+              className="search-card-info py-2"
+            >
+              {truncateText(selectedItem?.summary, 145)}
+            </Link>
+          </div>
+        </div>
+        :
+        ''
+      }
+      {articles?.filter(data=>{
+        return  data.id !== searchParams.get('id')
+      })?.map((data, index) => (
         <div className="pb-1 " style={{'cursor':'pointer'}}
         onClick={()=>{
           if(setArticle){
@@ -56,56 +118,21 @@ const ArticleAside = ({ articles ,setArticle}) => {
           }
         }}
         >
+          
           <div className={`search-result-card ${data.id === searchParams.get('id') ? "active" : ""}`}>
             <Link
-              // to={`/search/article/result/${data.id}`}
               className="search-card-title pb-3"
             >
               {data.title}
             </Link>
             <div class="search-card-profile">
-              {/* <div
-                class="dropdown-profile-menu collapse multi-collapse overflow-hidden"
-                role="menu"
-                id={"dataCollapseProfile" + index}
-                style={{
-                  width: "60%",
-                  top: "60%",
-                  left: "12%",
-                  transformOrigin: "left top",
-                  zIndex: 8,
-                }}
-              >
-                <div class="search-profile-menu py-2">
-                  {data.partnership.map((partner, index) => (
-                    <Link to="#" class="search-dropdown-item" key={partner._id}>
-                      <div class="search-dropdown-pic">
-                        <img
-                          src={
-                            !partner?.partnerId?.image
-                              ? profilepic
-                              : partner?.partnerId?.image
-                          }
-                          alt=".."
-                          className="search-dropdown-pic"
-                        />
-                      </div>
-                      <p class="search-dropdown-name">
-                        {partner?.partnerId?.first_name}{" "}
-                        {partner?.partnerId?.last_name}
-                      </p>
-                    </Link>
-                  ))}
-                </div>
-              </div> */}
+             
               <div class="search-card-flex">
                 <div class="search-card-pic-container">
                   <div class="search-card-pic">
                     <img
                       src={
-                        // data?.
                         profilepic
-                        // !data.authorId.image ? profilepic : data.authorId.image
                       }
                       data-bs-toggle="collapse"
                       href={"#dataCollapseProfile" + index}
@@ -116,13 +143,7 @@ const ArticleAside = ({ articles ,setArticle}) => {
                       className="img-fluid search-card-pic"
                     />
                   </div>
-                  {/* <div class="search-card-pic-num">
-                    {data.partnership.length}
-                  </div> */}
                 </div>
-                {/* <div class="search-card-name">
-                  {data?.first_name} {data?.authorId?.last_name}
-                </div> */}
               </div>
               <div class="search-card-amount">
                 {!data.price || data.price === 0 ? "Free" : `N ${data.price}`}
@@ -134,38 +155,6 @@ const ArticleAside = ({ articles ,setArticle}) => {
             >
               {truncateText(data.summary, 145)}
             </Link>
-
-            {/* <div class="search-card-icon-part">
-              <div class="search-card-icon-container">
-                <div class="search-card-count">{data.likes}</div>
-                <img class="search-card-count-icon" src={like} alt=".." />
-              </div>
-              <div class="search-card-icon-container">
-                <div class="search-card-count">{data.download}</div>
-                <img class="search-card-count-icon" src={download} alt=".." />
-              </div>
-              <div class="search-card-icon-container">
-                <div class="search-card-count">{data.share}</div>
-                <img class="search-card-count-icon" src={share} alt=".." />
-              </div>
-            </div>
-            <div className="d-flex justify-content-between pt-3">
-              <div className="search-card-star">
-                <div>
-                  {[...Array(Math.floor(data.rating))].map((star, index) => (
-                    <GoStarFill key={index} size={18} color=" #4eb473" />
-                  ))}
-                  {data.rating % 1 !== 0 && (
-                    <FaRegStarHalfStroke size={18} color="#4eb473" />
-                  )}
-                  {[...Array(5 - Math.ceil(data.rating))].map((_, index) => (
-                    <GoStar key={index} size={18} color="#4eb473" />
-                  ))}
-                </div>
-                <div className="search-card-rate">{data.rating} out of 5</div>
-              </div>
-              <div className="search-card-date">{formatDate(data.date)}</div>
-            </div> */}
           </div>
         </div>
       ))}
